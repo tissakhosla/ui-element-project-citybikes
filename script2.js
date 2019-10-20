1217709f87b0998c5f1754d4c7b50cc1e398e695
Tissa Khosla <tissa.music@gmail.com>
2019-10-20 18:19:31 -0400

    cleaning up search on build 2

--- script2.js
+++ script2.js
@@ -1,8 +1,24 @@
 console.log("Wea rel ive")
 
 const url3 = "http://api.citybik.es/v2/networks/capital-bikeshare"
+
 function bikes(stationArray) {
-    console.dir(stationArray)
+ 
+    const form = document.querySelector("form")
+    
+    form.addEventListener("submit", function(eo){
+        eo.preventDefault()
+        let main = document.querySelector(".page")
+        let searchText = document.querySelector(".searchtext")
+
+        for(let i = 0; i < stationArray.length; i++){
+            
+            if(stationArray[i].name.toUpperCase().includes(searchText.value.toUpperCase()))
+                main.appendChild(document.createElement("div")).innerHTML = stationArray[i].name
+        }
+
+    })
+
 }
 
 fetch(url3)
