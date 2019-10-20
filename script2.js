43f664a246fb1235be0dbf3e05768b24e10d6d62
Tissa Khosla <tissa.music@gmail.com>
2019-10-20 17:25:00 -0400

    build v2

--- script2.js
+++ script2.js
@@ -1,7 +1,12 @@
 console.log("Wea rel ive")
-const url = "http://api.citybik.es/v2/networks/capital-bikeshare"
 
-fetch(url)
+const url3 = "http://api.citybik.es/v2/networks/capital-bikeshare"
+function bikes(stationArray) {
+    console.dir(stationArray)
+}
+
+fetch(url3)
     .then(res => res.json())
-    .then(console.log(res))
-    .catch(err => "Something's up...", err)
+    .then(res => bikes(res.network.stations))
+    .catch(err => console.log("No More Matches!\n", err))
+
