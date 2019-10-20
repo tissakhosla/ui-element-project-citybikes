ae924b54b37646574b2cf74c1b4dfa107e345fa2
Tissa Khosla <tissa.music@gmail.com>
2019-10-19 20:51:56 -0400

    displaying table of info, gonna build search

--- script.js
+++ script.js
@@ -2,64 +2,67 @@ console.log("LIVE")
 
 const url = "http://api.citybik.es/v2/networks/"
 const whatCity = "Washington, DC"
-
-var stationData = function(stationObj){
-	console.log("these are these 6 things urls\n")
-	// for(let i = 0; i < 6; i++){
-		console.dir(stationObj.data.en.feeds[1].url)
-		let data0 = stationObj.data.en.feeds[1].url
-		fetch(data0)
-			// .then(res => res.json())
-			.then(console.dir(res))
-			.catch(err => console.log("uh oh"), err)
-}
+const table = document.querySelector("table")
 
 var findStation = function(networkObj, networkEndPoint){
 	console.log("networkEndPoint\n" + networkEndPoint)
 	console.log("networkObj")
 	console.dir(networkObj)
-	console.log("WHAT IS THIS??")
-	// console.dir(networkObj.network.stations)
-	let mysteryObj = networkObj.network.stations
-	console.dir(mysteryObj)
-	
+	console.log("stationArray")
+	let stationArray = networkObj.network.stations
+	console.dir(stationArray)
+	for(let i = 0; i < stationArray.length; i++){
+		let search = "14th"
+		if(stationArray[i].name == search)
+			let row = document.createElement("tr")
+			table.appendChild(row)
+			let rowName = document.createElement("td")
+			row.appendChild(rowName)
+			rowName.innerHTML = stationArray[i].name
+			let rowFree = document.createElement("td")
+			row.appendChild(rowFree)
+			rowFree.innerHTML = stationArray[i].free_bikes
+			let rowEmpty = document.createElement("td")
+			row.appendChild(rowEmpty)
+			rowEmpty.innerHTML = stationArray[i].empty_slots
+			let rowLat = document.createElement("td")
+			row.appendChild(rowLat)
+			rowLat.innerHTML = stationArray[i].latitude
+			let rowLong = document.createElement("td")
+			row.appendChild(rowLong)
+			rowLong.innerHTML = stationArray[i].longitude
+			let rowTime = document.createElement("td")
+			row.appendChild(rowTime)
+			rowTime.innerHTML = stationArray[i].timestamp
+		}
+	}
+
+
 	// Logs all station names without bikes
 	// for(let i = 0;i < mysteryObj.length; i++){
 	// 	if (mysteryObj[i].free_bikes === 0){
 	// 		console.dir(mysteryObj[i].name)
 	// 	}
-	
 	// console.dir(networkObj.network.gbfs_href)
-	let stationUrl = networkObj.network.gbfs_href
-	console.log(stationUrl)	
-
-	fetch(stationUrl)
-		.then(res => res.json())
-		.then(res => stationData(res))
-		.catch(err => console.log("ERROR in fetch 3\n", err))
 }
 
+var findNetwork = function(apiObj, apiStartPoint){			
+	for (i = 0; i < apiObj.networks.length; i++){
+		if (apiObj.networks[i].location.city === whatCity){
+			console.log("apiObj")
+			console.dir(apiObj.networks[i])
+			let networkId = apiObj.networks[i].id
+			// console.log(networkId) 
+			// console.log(url + networkId)
+			let networkUrl = apiStartPoint + networkId
+			console.log("networkUrl\n" + networkUrl)
 
-
-var findNetwork = function(apiObj, apiStartPoint){
-			
-			for (i = 0; i < apiObj.networks.length; i++){
-				if (apiObj.networks[i].location.city === whatCity){
-					console.log("apiObj")
-					console.dir(apiObj.networks[i])
-					let networkId = apiObj.networks[i].id
-					// console.log(networkId) 
-					// console.log(url + networkId)
-					let networkUrl = apiStartPoint + networkId
-					console.log("networkUrl\n" + networkUrl)
-
-					fetch(networkUrl)
-						.then(res => res.json())
-						.then(res => findStation(res, networkUrl))
-						.catch(err => console.log("No More Matches!\n", err))
-				} 
-
-			}
+			fetch(networkUrl)
+				.then(res => res.json())
+				.then(res => findStation(res, networkUrl))
+				.catch(err => console.log("No More Matches!\n", err))
+		} 
+	}
 }
 
 bikes = document.querySelector("button")
